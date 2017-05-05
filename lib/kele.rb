5e868f0946d539109f99dfead8462f68bfd0ac96
Meghana Singh <meghana1602@gmail.com>
2017-05-05 19:07:26 +0000

    Completed checkpoint

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,13 +1,18 @@
 require 'httparty'
 
 class Kele
-    
+  attr_accessor :auth_token, :user_data
+  
   include HTTParty
   
   def initialize(email, pwd)
-    @email = email
-    @pwd   = pwd
-    @response = self.class.post("https://www.bloc.io/api/v1/sessions", body:{email: @email, password: @pwd})
+    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {email: email, password: pwd})
+    @auth_token = response["auth_token"] 
   end
 
+  def get_me 
+    @user_data = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token }) 
+     
+    puts "name:  #{@user_data["name"]}" + "\n" + "email: #{@user_data["email"]}\n"  + "course name: #{@user_data["course"]["name"]}\n" 
+  end
 end
